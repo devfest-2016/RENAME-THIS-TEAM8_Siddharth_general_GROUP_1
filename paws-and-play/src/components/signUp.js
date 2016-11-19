f2495a1385740e4f6b5a8a218ba818c255e4ddd5
jaspercurry <jaspercurry@gmail.com>
2016-11-19 15:03:24 -0500

    modifed form

--- paws-and-play/src/components/signUp.js
+++ paws-and-play/src/components/signUp.js
@@ -5,24 +5,45 @@ export default function SignUp(){
 		<div>
       <form className="" action="index.html" method="post" >
         <fieldset>
-          <div className="form-group">
-            <input className="form-control input-md" type="text" name="username" value="" placeholder="User Name" />
+				<h2>User Info</h2>
+					<div className="form-group">
+            <input className="form-control input-md" type="text" name="name" value="" placeholder="Email" />
           </div>
           <div className="form-group">
             <input className="form-control input-md" type="password" name="username" value="" placeholder="Password" />
           </div>
+					<h2>Dog Info</h2>
           <div className="form-group">
             <input className="form-control input-md" type="text" name="dogname" value="" placeholder="Dog Name" />
           </div>
           <div className="form-group">
-            <input className="form-control input-md" type="text" name="name" value="" placeholder="Email" />
+            <input className="form-control input-md" type="text" name="name" value="" placeholder="Breed" />
           </div>
           <div className="form-group">
-            <input className="form-control input-md" type="text" name="name" value="" placeholder="something" />
+            <input className="form-control input-md" type="text" name="name" value="" placeholder="Age" />
           </div>
-          <div className="form-group">
-            <textarea className="form-control" placeholder="Description"></textarea>
-          </div>
+					<div className="form-group">
+						<label for="male">Temperment:</label>
+						<select>
+						  <option value="sweet">Nice</option>
+						  <option value="aggressive">Fuuny</option>
+						  <option value="playful">Aggressive</option>
+						  <option value="dumb">Dumb</option>
+						</select>
+					</div>
+					<div className="form-group">
+						<label for="male">Size:</label>
+						<select>
+						  <option value="xs">Extra Small</option>
+						  <option value="s">Small</option>
+						  <option value="m">Medium</option>
+						  <option value="lg">Large</option>
+						  <option value="xlg">Extra Large</option>
+						</select>
+					</div>
+					<div className="form-group">
+						<textarea className="form-control" placeholder="Description"></textarea>
+					</div>
           <div className="form-group">
             <button className="btn btn-lg btn-default dark-button">Sign Up</button>
           </div>
--- paws-and-play/src/components/welcome.js
+++ paws-and-play/src/components/welcome.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function welcome(){
 	return(
-		<div>
-			<h1>Paws & Play</h1>
-		</div>
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+            <div id="welcome-div">
+            <h1>Make Doggy Friends</h1>
+              <p class="">
+                See when and where local dogs love to walk!
+                </p>
+              <button class="btn btn-default btn-lg ">Learn more >></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 	)
 }
